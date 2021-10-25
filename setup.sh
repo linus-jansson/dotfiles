0f14fb9d9ca422eca9d8f2c52e2b4599b5784df7
Linus Jansson <linus.jansson2000@gmail.com>
2021-10-25 18:22:47 +0200

    added git pull

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+cd $SCRIPT_DIR && git pull
+
 ln -fs $SCRIPT_DIR/git/.gitconfig ~/.gitconfig
 ln -fs $SCRIPT_DIR/bash/.bashrc ~/.bashrc
 #ln -fs $SCRIPT_DIR/git/.gitconfig ~/.gitconfig
