36e20f8f8812b4d4140db67927cd12b88c5ff2a2
Linus Jansson <linus.jansson2000@gmail.com>
2021-10-25 21:23:39 +0200

    La till nvim till setup skriptet

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ cd $SCRIPT_DIR && git pull
 ln -fs $SCRIPT_DIR/git/.gitconfig ~/.gitconfig
 ln -fs $SCRIPT_DIR/bash/.bashrc ~/.bashrc
 
+ln -fs $SCRIPT_DIR/nvim/ ~/.config/
 ln -fs $SCRIPT_DIR/neofetch/ ~/.config/
 #ln -fs $SCRIPT_DIR/git/.gitconfig ~/.gitconfig
