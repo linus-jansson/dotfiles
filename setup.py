38fc38982ddea00560d30103a2859ce80ba04baf
Linus Jansson <linus.jansson2000@gmail.com>
2021-12-05 00:53:16 +0100

    Added a function which looks if there is a crontab set already

--- setup.py
+++ setup.py
@@ -2,7 +2,24 @@ import sys
 import os
 import shutil
 import getpass
+import subprocess
 
+def check_for_crontab():
+    # '-l', '>', '$PWD/tmp_crontab'
+    crontab_list = ['crontab', '-l']
+    pipe = subprocess.run(crontab_list, stdout=subprocess.PIPE)    
+    # print(pipe.stdout, len(pipe.stdout))
+    # If the length of the crontab pipe is larger than 0, it has content.     
+    if len(pipe.stdout) > 0:
+        # Write pipe to a local file as binary 
+        with open('tmp_crontab', 'a+b') as crontab:
+            # lines = crontab.readlines()
+            crontab.write(pipe.stdout)
+        # os.remove('tmp_crontab')
+
+        return True
+
+    return False
 
 def main():
 
@@ -15,7 +32,11 @@ def main():
         print('It seems like you are on a linux dist')
         os.system(f'bash {os.getcwd()}/setup-linux.sh')
         if auto_update_checker == "Y":
-           os.system('crontab updater')
+            if check_for_crontab():
+                print("Crontab already exists, saved content to 'tmp_crontab'")
+                # with open('tmp_crontab', 'a+b'):
+            else:
+                os.system('crontab updater')
 
     # WIP
     elif sys.platform == 'win32':
