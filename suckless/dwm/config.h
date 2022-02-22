4f5f4de06123733b427cd6deed7d6595c36b38ab
Linus Jansson <linus.jansson2000@gmail.com>
2022-02-22 19:11:31 +0100

    kitty och lite picom

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 7;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -11,16 +11,18 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_gray5[]       = "#222626";
 static const char col_cyan[]        = "#005577";
+static const char col_green[]       = "#0ab377";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_green, col_gray5,  col_green  },
 };
 
 /* tagging */
 /* https://fontawesome.com/v4/cheatsheet/ */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "" };
+static const char *tags[] = { "", " ", "_", "_", "_", "_", "_", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,7 +31,13 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+  { "discord",  NULL,       NULL,       1 << 7,       0,           -1 },
+  { "Spotify",  NULL,       NULL,       1 << 6,       0,           -1 },
+  { "Code",     NULL,       NULL,       1 << 1,       0,           -1 }
+
+
+
 };
 
 /* layout(s) */
@@ -59,8 +67,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "-f", dmenufont };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 
 /* INLAGT AV LIMPAN */
