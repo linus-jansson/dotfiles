49badc1aab18a148cf25afe250297260e0fcf219
Linus Jansson <linus.jansson2000@gmail.com>
2022-02-22 01:18:45 +0100

    kommentarer och lite ändringar

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -60,6 +60,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+
+/* INLAGT AV LIMPAN */
 static const char *togglemutecmd[] = { "amixer", "-D", "pulse", "sset", "Master", "toggle", NULL };
 static const char *volumeup[] = { "amixer", "-D", "pulse", "sset", "Master", "5%+", NULL };
 static const char *volumedown[] = { "amixer", "-D", "pulse", "sset", "Master", "5%-", NULL };
@@ -67,12 +69,14 @@ static const char *brightnessup[] = { "xbacklight", "-inc", "5"};
 static const char *brightnessdown[] = { "xbacklight", "-dec", "5"};
 
 #define XF86XK_AudioLowerVolume	0x1008FF11   /* Volume control down        */
-#define XF86XK_AudioMute	0x1008FF12   /* Mute sound from the system */
+#define XF86XK_AudioMute	0x1008FF12   	 /* Mute sound from the system */
 #define XF86XK_AudioRaiseVolume	0x1008FF13   /* Volume control up          */
-#define XF86XK_MonBrightnessUp   0x1008FF02  /* Monitor/panel brightness */
-#define XF86XK_MonBrightnessDown 0x1008FF03  /* Monitor/panel brightness */
+#define XF86XK_MonBrightnessUp   0x1008FF02  /* Monitor/panel brightness   */
+#define XF86XK_MonBrightnessDown 0x1008FF03  /* Monitor/panel brightness   */
+/* --- */
 
 
+// Bra att ha
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -98,11 +102,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-			     { 0,                            XF86XK_AudioMute, spawn,      {.v = togglemutecmd } },
-		     { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volumeup } },
-		     { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = volumedown } },
-		     { 0,                            XF86XK_MonBrightnessUp, spawn, {.v = brightnessup} },
-		     { 0,                            XF86XK_MonBrightnessDown, spawn, {.v = brightnessdown} },
+	{ 0,                            XF86XK_AudioMute, spawn,      {.v = togglemutecmd } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volumeup } },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = volumedown } },
+	{ 0,                            XF86XK_MonBrightnessUp, spawn, {.v = brightnessup} },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn, {.v = brightnessdown} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
